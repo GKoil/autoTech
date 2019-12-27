2bfc1407d287924847a13f5acb9e4b214afb8c0e
Кирилл <samsuu@ya.ru>
2019-12-27 16:10:38 +0700

    add uppdate

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,38 +1,89 @@
 let outputValue = 0;
-let arr = [];
-let i = -1;
 
 function priceCalculate () {
   const inputValue = document.getElementById("input-value").value;
-  outputValue = 16997 * inputValue - 1000000;
 
-
-  if (outputValue) {
-    i = i + 1;
-    arr[i] = outputValue;
+  if (inputValue < 60) {
+    return alert ("Введите значение больше или равное 60");
+  }  else if (inputValue > 900) {
+    return alert ("Введите значение меньшее или равное 900");
   }
+
+  const formula = 16997 * inputValue - 1000000;
+  outputValue = formula;
 
   const p = document.getElementById("out");
   p.innerHTML = `${outputValue.toLocaleString()} ₽`;
 
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: 'line',
+  drawingGraphic(inputValue, outputValue);
 
-      // The data for our dataset
-      data: {
-          labels: ['100', '200', '300', '400', '500', '600', '700'],
-          datasets: [{
-              label: 'Цена автомобиля',
-              backgroundColor: '#007bff',
-              borderColor: '#007bff',
-              data: arr
-          }]
-      },
+  const countGraphic = document.getElementById("graphic-count");
+  countGraphic.innerHTML = "Стоимость";
+  const powerGraphic = document.getElementById("graphic-power");
+  powerGraphic.innerHTML = "Мощность двигателя";
+}
 
-      // Configuration options go here
-      options: {}
+function drawingGraphic (inputValue, graphicValue) {
+  var speedCanvas = document.getElementById("myChart");
+
+  Chart.defaults.global.defaultFontFamily = "Lato";
+  Chart.defaults.global.defaultFontSize = 18;
+  
+  var dataFirst = {
+      label: "Статистика",
+      data: [
+        {
+          x: 100,
+          y: 1000000
+        },
+        {
+          x: 900,
+          y: 9000000
+        }
+      ],
+      lineTension: 0,
+      fill: false,
+      borderColor: 'blue'
+    };
+  
+  var dataSecond = {
+      label: "Выбранное значение",
+      data: [{
+        x: inputValue,
+        y: graphicValue
+      }],
+      lineTension: 0,
+      fill: false,
+    borderColor: 'red',
+    borderWidth: 14
+    };
+  
+  var speedData = {
+    labels: ["0", "100", "200", "300", "400", "500", "600", "700"],
+    datasets: [dataFirst, dataSecond]
+  };
+  
+  var chartOptions = {
+    scales: {
+      xAxes: [{
+          type: 'linear',
+          position: 'bottom'
+      }]
+    },
+    legend: {
+      display: true,
+      position: 'top',
+      labels: {
+        boxWidth: 80,
+        fontColor: 'black'
+      }
+    }
+  };
+  
+  var lineChart = new Chart(speedCanvas, {
+    type: 'line',
+    data: speedData,
+    options: chartOptions
   });
 }
 
